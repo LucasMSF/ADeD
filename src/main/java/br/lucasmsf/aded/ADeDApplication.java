8c640848bd75615e9e3c189f337ae119a33b8033
LucasMSF <lucasgg003@gmail.com>
2023-09-10 01:57:38 -0300

    Initial Commit

---
+++ src/main/java/br/lucasmsf/aded/ADeDApplication.java
@@ -0,0 +1,13 @@
+package br.lucasmsf.aded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ADeDApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ADeDApplication.class, args);
+    }
+
+}
